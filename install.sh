7a86372cfc498a13d93d986a5a0745787bfbd751
htkael <htkael@gmail.com>
2025-07-01 22:18:41 -0500

    add bin dir in install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -sf ~/dotfiles/shell_functions ~/.shell_functions
 ln -sf ~/dotfiles/shell_exports ~/.shell_exports
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 mkdir -p ~/.config/alacritty
+mkdir -p ~/bin/
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 echo "Dotfiles installed!"
